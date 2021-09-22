b3683dcb7a7b72f5bfeba24eaea1493d4b5f9151
neil-marcellini <neil.marcellini@gmail.com>
2021-09-22 15:10:21 -0700

    mobile navigation

--- components/site-nav.js
+++ components/site-nav.js
@@ -1,17 +1,32 @@
+
 fetch("/components/site-nav.html")
     .then(stream => stream.text())
-    .then(text => define(text));
-
-function define(html) {
-    const template = document.createElement('template')
-    template.innerHTML = html
+    .then(text => define(text))
+const define = (html) => {
   class SiteNav extends HTMLElement {
       constructor() {
           super();
-          this.attachShadow({mode: 'open'});
-          this.shadowRoot.appendChild(template.content.cloneNode(true))
+          let shadow = this.attachShadow({mode: 'open'});
+          shadow.innerHTML = html;
+          addMenuListener(shadow)
       }
   }
 
-  customElements.define('site-nav', SiteNav);
+  window.customElements.define('site-nav', SiteNav);
+}
+
+const addMenuListener = (shadow) => {
+    const burger = shadow.querySelector('.hamburger-icon')
+    const closeIcon = shadow.querySelector('.close-icon')
+    const nav = shadow.querySelector('.nav-links')
+    const slide = shadow.querySelector('.slide-bar')
+
+    const toggleMenu = () => {
+        nav.classList.toggle('nav-active')
+        slide.classList.toggle('nav-active')
+    }
+
+    burger.addEventListener('click', toggleMenu)
+    closeIcon.addEventListener('click', toggleMenu)
+
 }
