ef71826e6a7bc92d8acf97a55ebb639f7f4b6287
neil-marcellini <neil.marcellini@gmail.com>
2021-10-06 21:17:13 -0700

    Styled arrows and added circles

--- components/image-slider.js
+++ components/image-slider.js
@@ -8,8 +8,26 @@ class ImageSlider extends HTMLElement {
       super();
       let shadow = this.attachShadow({mode: 'open'});
       shadow.innerHTML = html;
+      addCircles(shadow)
   }
 }
 
 window.customElements.define('image-slider', ImageSlider);
+}
+
+const addCircles = (shadow) => {
+  let slot = shadow.querySelector('slot')
+  let images = slot.getElementsByTagName('img')
+  let container = shadow.querySelector('.container')
+  // create circles container
+  let circles = document.createElement('div')
+  container.appendChild(circles)
+  circles.className = 'circles'
+  for (let image in images) {
+    // create circle
+    let circle = document.createElement('div')
+    circle.className = 'circle'
+    // add circle
+    circles.appendChild(circle)
+  }
 }
